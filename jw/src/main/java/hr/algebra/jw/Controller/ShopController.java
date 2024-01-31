0b4c25d20cbb1b8ae25bb83e1ec51af13437c787
antebaskovic <119291043+antebaskovic@users.noreply.github.com>
2024-01-31 11:52:20 +0100

    shop start

--- jw/src/main/java/hr/algebra/jw/Controller/ShopController.java
+++ jw/src/main/java/hr/algebra/jw/Controller/ShopController.java
@@ -1,27 +1,50 @@
 package hr.algebra.jw.Controller;
 
-import hr.algebra.jw.Dto.UserDto;
-import hr.algebra.jw.Services.UserService;
+import hr.algebra.jw.Model.Product;
+import hr.algebra.jw.Repositories.ProductRepository;
+import hr.algebra.jw.Services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.security.Principal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
+@RequestMapping("/shop")
 public class ShopController {
 
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private ProductService productService;
 
-    @GetMapping("/shop")
-    public String showRegistrationForm() {
-        return "shop";
+    @GetMapping("")
+    public String showProducts(Model model) {
+
+
+        List<Product> products = productRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+
+        Map<Long, Integer> productQuantities;
+        productQuantities = new HashMap<>();
+
+        for (Product product : products) {
+            productQuantities.put(product.getId(), 1);
+        }
+
+        model.addAttribute("products", products);
+        model.addAttribute("productQuantities", productQuantities);
+
+        System.out.println(productQuantities);
+        System.out.println(products);
+
+        return "shop/index";
     }
-
-
-
 }
+
+
+
